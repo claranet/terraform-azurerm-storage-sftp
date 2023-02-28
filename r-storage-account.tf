ea646d706d9e26a35d7d3f4b4635a6f65e552d56
Thierry Lei <thierry.lei@fr.clara.net>
2023-02-28 18:51:53 +0100

    AZ-924: Init module

---
+++ r-storage-account.tf
@@ -0,0 +1,61 @@
+module "storage_account" {
+  # source  = "claranet/storage-account/azurerm"
+  # version = "~> 7.5.0"
+
+  source = "git@git.fr.clara.net:claranet/projects/cloud/azure/terraform/modules/storage-account.git?ref=AZ-924_add_sftp_support"
+
+  location       = var.location
+  location_short = var.location_short
+  client_name    = var.client_name
+  environment    = var.environment
+  stack          = var.stack
+
+  resource_group_name = var.resource_group_name
+
+  use_caf_naming = var.use_caf_naming
+  name_prefix    = var.name_prefix
+  name_suffix    = var.name_suffix
+
+  storage_account_custom_name     = var.custom_storage_account_name
+  custom_diagnostic_settings_name = var.custom_diagnostic_settings_name
+
+  access_tier              = var.access_tier
+  account_kind             = var.is_premium ? "BlockBlobStorage" : "StorageV2"
+  account_tier             = var.is_premium ? "Premium" : "Standard"
+  account_replication_type = var.account_replication_type
+
+  https_traffic_only_enabled  = var.https_traffic_only_enabled
+  public_nested_items_allowed = var.public_nested_items_allowed
+  shared_access_key_enabled   = var.shared_access_key_enabled
+  min_tls_version             = var.min_tls_version
+
+  static_website_config = var.static_website_config
+
+  sftp_enabled  = true
+  nfsv3_enabled = var.nfsv3_enabled
+
+  containers = var.containers
+
+  storage_blob_data_protection = var.storage_blob_data_protection
+  storage_blob_cors_rule       = var.storage_blob_cors_rule
+
+  advanced_threat_protection_enabled = var.advanced_threat_protection_enabled
+
+  network_rules_enabled   = var.network_rules_enabled
+  default_firewall_action = var.default_firewall_action
+  subnet_ids              = var.subnet_ids
+  allowed_cidrs           = var.allowed_cidrs
+  network_bypass          = var.network_bypass
+
+  identity_type = var.identity_type
+  identity_ids  = var.identity_ids
+
+  logs_destinations_ids   = var.logs_destinations_ids
+  logs_categories         = var.logs_categories
+  logs_metrics_categories = var.logs_metrics_categories
+  logs_retention_days     = var.logs_retention_days
+
+  default_tags_enabled = var.default_tags_enabled
+
+  extra_tags = var.extra_tags
+}
